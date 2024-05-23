bcda52584f6cd31cc4deb3161797542565f35203
jaschdoc <60233376+jaschdoc@users.noreply.github.com>
2024-05-23 16:56:25 +0200

    refactor: impl FromStr for Envelope

--- src/envelope.rs
+++ src/envelope.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use serde::Deserialize;
 
 #[derive(Clone, Deserialize)]
@@ -19,11 +21,15 @@ pub struct Envelope {
 }
 
 impl Envelope {
-    pub fn from_str(json: &str) -> Result<Self, &'static str> {
-        serde_json::from_str(json).map_err(|_| "Unable to parse envelope from json")
-    }
-
     pub fn from_bytes(bytes: &[u8]) -> Result<Self, &'static str> {
         serde_json::from_slice(bytes).map_err(|_| "Unable to parse envelope from json")
+    }
+}
+
+impl FromStr for Envelope {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s).map_err(|_| "Unable to parse envelope from json")
     }
 }
